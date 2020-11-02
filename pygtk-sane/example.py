5ed7e0ac784da0a3f33fceae05db3d4e45ef4304
Jeremy A Gray <jeremy.a.gray@gmail.com>
2020-11-01 22:51:02 -0600

    chore: add license (GPLv3)
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- pygtk-sane/example.py
+++ pygtk-sane/example.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
+#
+# pygtk-sane:  a Python/GTK SANE front-end
+# Copyright (C) 2020 Jeremy A Gray <jeremy.a.gray@gmail.com>.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import gi
 
@@ -28,6 +44,12 @@ class PGSaneWindow(Gtk.Window):
     def on_button_click(self, widget, msg):
         print(msg)
 
+
+# For 'About.'
+# pygtk-sane  Copyright (C) 2020 Jeremy A Gray <jeremy.a.gray@gmail.com>
+# This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+# This is free software, and you are welcome to redistribute it
+# under certain conditions; type `show c' for details.
 
 win = PGSaneWindow()
 win.connect("destroy", Gtk.main_quit)
