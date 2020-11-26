b1711d14944520a050216b39441f5a9893f34bf3
Jeremy A Gray <jeremy.a.gray@gmail.com>
2020-11-26 11:18:18 -0600

    fix: add more GTK elements
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

--- pygtk-sane/example.py
+++ pygtk-sane/example.py
@@ -17,16 +17,38 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import gi
+import sane
+from PIL import Image
 
 gi.require_version("Gtk", "3.0")
 
 from gi.repository import Gtk
 
 
+def init():
+    # Initialize.
+    sane.init()
+
+
+def get_devices():
+    # Try to find the devices.
+    try:
+        return sane.get_devices()
+    except _sane.error:
+        print('no devices found')
+    finally:
+        sane.exit()
+
+
+def get_device_names():
+    names = []
+    for device in get_devices():
+        names.append(device[0])
+
+
 class PGSaneWindow(Gtk.Window):
     def __init__(self):
-        # super().__init__(self, title="Hello World")
-        super().__init__(title="Hello World")
+        super().__init__(title="pyGTK-SANE")
 
         self.box = Gtk.Box(spacing=6)
         self.add(self.box)
@@ -38,18 +60,23 @@ class PGSaneWindow(Gtk.Window):
                                   self.on_button_click, 'hello')
         self.button_world.connect("clicked",
                                   self.on_button_click, 'world')
+
+        self.select_device_combo = Gtk.ComboBoxText()
+        self.select_device_combo.set_entry_text_column(0)
+        self.select_device_combo.connect('changed', self.select_device, 'hi')
+        # self.select_device_combo.append_text(get_device_names())
+        self.select_device_combo.append_text(('Fujitsu ScanSnap', 'Fujitsu', 'ix500', 'ScanSnap')[0])
+
         self.box.pack_start(self.button_hello, True, True, 0)
         self.box.pack_start(self.button_world, True, True, 0)
+        self.box.pack_start(self.select_device_combo, True, True, 1)
 
     def on_button_click(self, widget, msg):
         print(msg)
 
+    def select_device(self, widget, msg):
+        print(msg)
 
-# For 'About.'
-# pygtk-sane  Copyright (C) 2020 Jeremy A Gray <jeremy.a.gray@gmail.com>
-# This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
-# This is free software, and you are welcome to redistribute it
-# under certain conditions; type `show c' for details.
 
 win = PGSaneWindow()
 win.connect("destroy", Gtk.main_quit)
