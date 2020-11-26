a270d1cad78d45d751c0210d5cc5919a47cc0dd2
Jeremy A Gray <jeremy.a.gray@gmail.com>
2020-11-26 11:16:24 -0600

    chore: add pytest and poetry support
    Signed-off-by: Jeremy A Gray <jeremy.a.gray@gmail.com>

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((python-mode
+  (flycheck-python-flake8-executable . "/home/gray/.virtualenvs/pygtk-sane/bin/flake8")
+  (flycheck-python-pylint-executable . "/home/gray/.virtualenvs/pygtk-sane/bin/flake8")))
